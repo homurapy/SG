e9c9611c595963e9de1e61804f89447636c4034e
Alex <2640178@gmail.com>
2021-02-03 11:53:28 +0300

    Libgdx.lesson1 Добавлен фон для игры

--- core/src/mygdx/game/StarGame.java
+++ core/src/mygdx/game/StarGame.java
@@ -7,21 +7,30 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class StarGame extends ApplicationAdapter {
+
 	SpriteBatch batch;
 	Texture img;
+	SpriteBatch spriteBatch;
+	Texture img1;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		spriteBatch = new SpriteBatch();
+		img1 = new Texture("backFon.jpg");
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+//		Gdx.gl.glClearColor(1, 0, 0, 1);
+//		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		spriteBatch.begin();
+		spriteBatch.draw(img1, 0,0, Gdx.graphics.getWidth(),Gdx.graphics.getWidth());
+		spriteBatch.end();
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, Gdx.graphics.getWidth()/2, Gdx.graphics.getWidth()/2);
 		batch.end();
 	}
 	
