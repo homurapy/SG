531f5e10d094ba5a8dbf22d2a0e3612ad1bdbc2f
Alex <2640178@gmail.com>
2021-02-11 09:33:10 +0300

    добавление структуры

--- core/src/mygdx/game/base/BaseScreen.java
+++ core/src/mygdx/game/base/BaseScreen.java
@@ -4,16 +4,37 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Matrix3;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Vector2;
+
+import mygdx.game.math.MatrixUtils;
+import mygdx.game.math.Rect;
 
 public class BaseScreen implements Screen, InputProcessor {
 
     protected SpriteBatch batch;
 
+    private Rect screenBounds;
+    private Rect glBounds;
+    private Rect worldBounds;
+
+    private Matrix4 worldToGl;
+    private Matrix3 screenToWorld;
+
+    private Vector2 touch;
+
     @Override
     public void show() {
         System.out.println("show");
-        batch = new SpriteBatch();
         Gdx.input.setInputProcessor(this);
+        batch = new SpriteBatch();
+        screenBounds = new Rect();
+        glBounds = new Rect(0, 0, 1f, 1f);
+        worldBounds = new Rect();
+        worldToGl = new Matrix4();
+        screenToWorld = new Matrix3();
+        touch = new Vector2();
     }
 
     @Override
@@ -24,6 +45,22 @@ public class BaseScreen implements Screen, InputProcessor {
     @Override
     public void resize(int width, int height) {
         System.out.println("resize width = " + width + " height = " + height);
+        screenBounds.setSize(width, height);
+        screenBounds.setLeft(0);
+        screenBounds.setBottom(0);
+
+        float aspect = width / (float) height;
+        worldBounds.setHeight(1f);
+        worldBounds.setWidth(1f * aspect);
+
+        MatrixUtils.calcTransitionMatrix(worldToGl, worldBounds, glBounds);
+        batch.setProjectionMatrix(worldToGl);
+        resize(worldBounds);
+        MatrixUtils.calcTransitionMatrix(screenToWorld, screenBounds, worldBounds);
+    }
+
+    public void resize(Rect worldBounds) {
+
     }
 
     @Override
@@ -69,18 +106,39 @@ public class BaseScreen implements Screen, InputProcessor {
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         System.out.println("touchDown screenX = " + screenX + " screenY = " + screenY);
+        touch.set(screenX, Gdx.graphics.getHeight() - screenY).mul(screenToWorld);
+        touchDown(touch, pointer, button);
+        return false;
+    }
+
+    public boolean touchDown(Vector2 touch, int pointer, int button) {
+        System.out.println("touchDown touchX = " + touch.x + " touchY = " + touch.y);
         return false;
     }
 
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
         System.out.println("touchUp screenX = " + screenX + " screenY = " + screenY);
+        touch.set(screenX, Gdx.graphics.getHeight() - screenY).mul(screenToWorld);
+        touchUp(touch, pointer, button);
+        return false;
+    }
+
+    public boolean touchUp(Vector2 touch, int pointer, int button) {
+        System.out.println("touchUp touchX = " + touch.x + " touchY = " + touch.y);
         return false;
     }
 
     @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
         System.out.println("touchDragged screenX = " + screenX + " screenY = " + screenY);
+        touch.set(screenX, Gdx.graphics.getHeight() - screenY).mul(screenToWorld);
+        touchDragged(touch, pointer);
+        return false;
+    }
+
+    public boolean touchDragged(Vector2 touch, int pointer) {
+        System.out.println("touchUp touchX = " + touch.x + " touchY = " + touch.y);
         return false;
     }
 
@@ -90,15 +148,11 @@ public class BaseScreen implements Screen, InputProcessor {
     }
 
     @Override
-    public boolean scrolled(float amount, float amountY) {
-        //        System.out.println("scrolled amount = " + amount);
-
+    public boolean scrolled(float amountX, float amountY) {
         return false;
     }
 
-//    @Override
-//    public boolean scrolled(int amount) {
-//        System.out.println("scrolled amount = " + amount);
-//        return false;
-//    }
+    public Matrix3 getScreenToWorld() {
+        return screenToWorld;
+    }
 }
