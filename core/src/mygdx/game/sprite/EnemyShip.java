7cc1049dd53fc111e280b0d0f2a1f0f6e8c18c37
Alex <2640178@gmail.com>
2021-02-23 16:36:07 +0300

    HomeWork.Lesson6

---
+++ core/src/mygdx/game/sprite/EnemyShip.java
@@ -0,0 +1,62 @@
+package mygdx.game.sprite;
+
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+import mygdx.game.base.Ship;
+import mygdx.game.math.Rect;
+import mygdx.game.pool.BulletPool;
+
+public class EnemyShip extends Ship {
+    private static final float START_SPEED = -0.25f;
+
+    public EnemyShip(BulletPool bulletPool, Rect worldBounds, Sound sound) {
+        this.bulletPool = bulletPool;
+        this.worldBounds = worldBounds;
+        this.sound = sound;
+        v = new Vector2();
+        v0 = new Vector2();
+        bulletPos = new Vector2();
+        bulletV = new Vector2();
+    }
+
+    @Override
+    public void update(float delta) {
+        if (getTop() > worldBounds.getTop()) {
+            reloadTimer = 0.7f *reloadInterval;
+        }
+        else {
+            v.set(v0);
+        }
+
+        super.update(delta);
+
+        bulletPos.set(pos.x, pos.y - getHalfHeight());
+        if (getBottom() < worldBounds.getBottom()) {
+            destroy();
+        }
+    }
+
+    public void set(
+            TextureRegion[] regions,
+            Vector2 v0,
+            TextureRegion bulletRegion,
+            float bulletHeight,
+            Vector2 bulletV,
+            int damage,
+            float reloadInterval,
+            float height,
+            int hp
+    ) {
+        this.regions = regions;
+        this.v0.set(v0);
+        this.bulletRegion = bulletRegion;
+        this.bulletHeight = bulletHeight;
+        this.bulletV.set(bulletV);
+        this.damage = damage;
+        this.reloadInterval = reloadInterval;
+        setHeightProportion(height);
+        this.hp = hp;
+        this.v.set(0,START_SPEED);
+    }
+}
